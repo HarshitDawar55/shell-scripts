a1cf7223f03bdbb4cc50e58c43e8cea9ced058d9
Harshit Dawar <31154755+HarshitDawar55@users.noreply.github.com>
2021-04-22 01:21:14 +0530

    Updated the code & removed the issue of runc!

--- docker-centos-installation.sh
+++ docker-centos-installation.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-dnf install docker-ce --nobest -y
-systemctl enable docker --now
+echo [Docker-CE] > /etc/yum.repos.d/docker-ce.repo
+echo  baseurl=https://download.docker.com/linux/centos/7/x86_64/stable/ >> /etc/yum.repos.d/docker-ce.repo
+echo gpgcheck=0 >> /etc/yum.repos.d/docker-ce.repo
+
+dnf install -y --nobest docker-ce
+systemctl enable --now docker
